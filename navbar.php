fe6e7392991bd4daeeeb65a8b6c3801b75b4a105
Vignesh <VigneshPrabha0209@gmail.com>
2018-10-22 16:55:55 +0530

    Made Better

--- navbar.php
+++ navbar.php
@@ -123,11 +123,11 @@
 
 
 
-<div class="modal fade" id="Modal1" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
 				  <div class="modal-dialog" role="document">
 				    <div class="modal-content">
 				      <div class="modal-header">
-				        <h5 class="modal-title" id="exampleModalLabel"></h5>
+				        <h5 class="modal-title" id="myModal"></h5>
 				        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 				          <span aria-hidden="true">&times;</span>
 				        </button>
