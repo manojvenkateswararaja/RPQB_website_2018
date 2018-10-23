9f05d630ed6a5298d81be2974b7aa2aa816398a1
Vignesh <VigneshPrabha0209@gmail.com>
2018-10-23 10:34:54 +0530

    Made Better

--- aboutus.php
+++ aboutus.php
@@ -573,11 +573,11 @@
 
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon mr_86" style="background-color:grey;" aria-hidden="true"></span>
+                <span class="fa fa-angle-left mr_86" style="background-color:grey;" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
             <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon ml_86" style="background-color:grey;margin-left: 76%;"
+                <span class="fa fa-angle-right ml_86" style="background-color:grey;margin-left: 76%;"
                     aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
--- blog.php
+++ blog.php
@@ -500,11 +500,11 @@
                   </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon mr_86" style="background-color:grey;" aria-hidden="true"></span>
+                  <span class="fa fa-angle-left mr_86" style="background-color:grey;" aria-hidden="true"></span>
                   <span class="sr-only">Previous</span>
                 </a>
                 <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon ml_86" style="background-color:grey;" aria-hidden="true"></span>
+                  <span class="fa fa-angle-right ml_86" style="background-color:grey;" aria-hidden="true"></span>
                   <span class="sr-only">Next</span>
                 </a>
               </div>
